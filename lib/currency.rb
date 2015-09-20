104224396c9cd217adeb96ba7112f07b7cadeeef
Marcos Serpa <marcosserpa@gmail.com>
2015-09-20 12:49:56 -0300

    Adds the main - maybe the ones, young padawan - instance and class specific methods

--- lib/currency.rb
+++ lib/currency.rb
@@ -1,5 +1,54 @@
 require "currency/version"
+require 'byebug'
 
 module Currency
-  # Your code goes here...
+
+  @@rates = {}
+  @@base = 'BRL'
+
+  class Money
+
+    attr_accessor :amount, :currency
+
+    class << self
+
+      # class methods ===========
+      def conversion_rates(currency_base = '', rates = {})
+        if currency_base.empty? && rates.empty?
+          raise ArgumentError, "Entry's format must be 'BASE_CURRENCY', { 'CONVERSION_RATE_1' => RATE_1,
+            'CONVERSION_RATE_2' => RATE_2, etc... }"
+        end
+
+        @@base = currency_base
+
+        unless rates.empty?
+          @@rates = rates
+        end
+      end
+
+      def rates
+        @@rates
+      end
+
+    end
+
+    # instance methods ===========
+    def initialize(amount = 0, currency = @@base)
+      @amount = amount
+      @currency = currency
+    end
+
+    def convert_to(currency)
+      if !@@rates.empty? && @@rates.include?(currency)
+        value = amount * @@rates[currency]
+      else
+        "Currency rate not defined"
+      end
+    end
+
+    def inspect
+      "#{ amount.to_f } #{ currency }"
+    end
+  end
+
 end
--- spec/currency_spec.rb
+++ spec/currency_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
 describe Currency do
-  
+
+  describe '#new' do
+    context "when creating an object" do
+      money = Currency::Money.new(10, 'EUR')
+
+      it { expect(money.amount).to eq(10) }
+      it { expect(money.currency).to eq('EUR') }
+    end
+  end
+
+  describe '#convert_to' do
+    Currency::Money.conversion_rates('BRL', { 'USD' => 3.88 })
+    let (:money) { Currency::Money.new(10, 'BRL') }
+
+    context "when currency is defined" do
+      it { expect(money.convert_to('USD')).to eq(38.8) }
+    end
+
+    context "when currency is not defined" do
+      it { expect(money.convert_to('EUR')).to eq("Currency rate not defined") }
+    end
+  end
+
+  describe '.conversion_rates' do
+    context "whe entry has no arguments" do
+      it { expect{ Currency::Money.conversion_rates() }.to raise_error(ArgumentError) }
+    end
+
+    context "when entry is complete" do
+      Currency::Money.conversion_rates('EUR', { 'USD' => 3.88 })
+
+      it { expect(Currency::Money.rates).to match('USD' => 3.88) }
+    end
+  end
+
+  describe '#inspect' do
+    context "when inspected" do
+      money = Currency::Money.new(10, 'BRL')
+
+      it { expect(money.inspect).to eq("10.0 BRL") }
+    end
+  end
 end
